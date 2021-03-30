087ed74944c32cd90922cc0d53bf3dcf8310877d
omkar9405 <omkarp1101@rediffmail.com>
2021-03-30 17:19:56 +0530

    taskerLogin

--- src/app/employees/loginemployee/loginemployee.component.ts
+++ src/app/employees/loginemployee/loginemployee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/_services/authentication.service';
 
 @Component({
   selector: 'app-loginemployee',
@@ -6,10 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loginemployee.component.css']
 })
 export class LoginemployeeComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+  success='';
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+) { 
+    // redirect to home if already logged in
+    if (this.authenticationService.currentCustomerValue) { 
+        this.router.navigate(['/customerPrifile']);
+    }
+}
 
-  ngOnInit(): void {
-  }
+ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+   // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/customerProfle';
+}
+
+// convenience getter for easy access to form fields
+get f() { return this.loginForm.controls; }
+
+onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+        return;
+    }
+    this.error='';
+    this.loading = true;
+    this.authenticationService.login(this.f.email.value, this.f.password.value)
+        .subscribe(
+            data => {
+                alert("Login Successful..");
+                console.log(localStorage.getItem('Token'));
+                this.router.navigate(['/customerProfile']);
+               
+            },
+            error => {
+                this.error = error;
+                this.loading = false;
+            });
+}
+
 
 }
--- src/app/employees/signup-employee/signup-employee.component.ts
+++ src/app/employees/signup-employee/signup-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/_services/authentication.service';
 
 @Component({
   selector: 'app-signup-employee',
@@ -7,9 +10,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupEmployeeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  signupForm: FormGroup;
+  success='';
+  taskerDto = {
+   "customername":"",
+   "mobile": 0,
+   "gender":"Select Gender",
+   "email": "",
+   "dob":"",
+   "password": "",
+   "signupcondition":"Accepted"
   }
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+  constructor(
+    private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService
+  ) { }
+ 
+ 
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      customername:['',Validators.required],
+      mobile:['',Validators.required],
+      gender:['',Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      dob:['',Validators.required],
+      signupcondition:['',Validators.required]
+  });
+  }
+
+  get f() { return this.signupForm.controls; }
+
+  register() {
+    this.submitted = true;
+
+
+        this.loading = true;
+    this.authenticationService.signup(this.taskerDto).subscribe(
+      (data:any) => {
+        console.log(data);
+        this.success='success';
+        alert("Registered Successfully");
+        this.clearAll();
+       
+    },(err) => {
+        alert(err);
+        this.error=err;
+        this.loading =false;
+    
+    });
+  }
+
+  clearAll()
+  {
+    this.taskerDto.customername="",
+    this.taskerDto.mobile= 0,
+    this.taskerDto.gender="",
+    this.taskerDto.email= "",
+    this.taskerDto.dob="",
+    this.taskerDto.password= ""
+  }
+
 
 }
