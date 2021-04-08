915dc704d7f77436683923b8a3cf9c2721283782
omkar9405 <omkarp1101@rediffmail.com>
2021-04-08 18:46:56 +0530

    tasker

--- src/app/employees/employeeprofile/employeeprofile.component.ts
+++ src/app/employees/employeeprofile/employeeprofile.component.ts
@@ -2,6 +2,7 @@ import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TaskerService } from 'src/app/_services/tasker.service';
+import { DatatableService } from 'src/app/_services/datatableservice/datatable.service';
 
 @Component({
   selector: 'app-employeeprofile',
@@ -11,9 +12,10 @@ import { TaskerService } from 'src/app/_services/tasker.service';
 export class EmployeeprofileComponent implements OnInit {
   
 
-  constructor(private taskerService:TaskerService,   private router: Router,) { }
+  constructor(private taskerService:TaskerService,  private datatableservice: DatatableService,  private router: Router,) { }
 
   ngOnInit(): void {
+    this.datatableservice.initTable('customers');
     console.log(localStorage.getItem('currentTasker'));
   }
 
