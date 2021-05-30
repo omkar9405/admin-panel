136b91111dfdce6c99cc7fa4fb0d25ad3c4a0252
omkar9405 <omkarp1101@rediffmail.com>
2021-05-31 03:19:36 +0530

    forgotpassword

--- src/app/customer/login/forgotpassword-customer/forgotpassword-customer.component.ts
+++ src/app/customer/login/forgotpassword-customer/forgotpassword-customer.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormGroup , FormBuilder, Validators  } from '@angular/forms';
+import { AuthenticationService } from 'src/app/_services/authentication.service';
 @Component({
   selector: 'app-forgotpassword-customer',
   templateUrl: './forgotpassword-customer.component.html',
   styleUrls: ['./forgotpassword-customer.component.css']
 })
 export class ForgotpasswordCustomerComponent implements OnInit {
-
-  constructor(private location: Location) { }
+  forgotForm:FormGroup
+  loadingOTP = false;
+  loadingforgot = false;
+  submittedEmail = false;
+  error = '';
+  success='';
+  constructor(private location: Location,private formBuilder:FormBuilder, private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.forgotForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      OTP: ['', Validators.required]
+  });
   }
+  get f() { return this.forgotForm.controls; }
+
+  forgotPassword(){
+    this.loadingforgot = true;
+  }
+sendOTP()
+{
+  this.loadingOTP = true;
+  
+}
 
   back()
 { 
--- src/app/employees/loginemployee/forgotpassword-tasker/forgotpassword-tasker.component.ts
+++ src/app/employees/loginemployee/forgotpassword-tasker/forgotpassword-tasker.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TaskerService } from 'src/app/_services/tasker.service';
 @Component({
   selector: 'app-forgotpassword-tasker',
   templateUrl: './forgotpassword-tasker.component.html',
@@ -7,10 +9,31 @@ import { Location } from '@angular/common';
 })
 export class ForgotpasswordTaskerComponent implements OnInit {
 
-  constructor(private location: Location) { }
+  forgotForm:FormGroup
+  loadingOTP = false;
+  loadingforgot = false;
+  submittedEmail = false;
+  error = '';
+  success='';
+  constructor(private location: Location,private formBuilder:FormBuilder, private taskerService: TaskerService) { }
 
   ngOnInit(): void {
+    this.forgotForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      OTP: ['', Validators.required]
+  });
   }
+
+  get f() { return this.forgotForm.controls; }
+  
+  forgotPassword(){
+    this.loadingforgot = true;
+  }
+sendOTP()
+{
+  this.loadingOTP = true;
+  
+}
 
   back()
 { 
